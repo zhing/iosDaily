5d40e60227042aebe1d55732efdb5483d86ef939
zhangqing <qzhang@linkedin.com>
2016-04-15 18:22:11 +0800

    add drawRect viewtest

---
+++ Tarbar/Tarbar/DrawView.h
@@ -0,0 +1,13 @@
+//
+//  DrawView.h
+//  Tarbar
+//
+//  Created by Qing Zhang on 4/15/16.
+//  Copyright © 2016 zhing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DrawView : UIView
+
+@end
