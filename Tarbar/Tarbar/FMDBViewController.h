a102c6242894996af4d9f95fbacafc848656b129
zhangqing <qzhang@linkedin.com>
2016-05-04 14:30:17 +0800

    add FMDB

---
+++ Tarbar/Tarbar/FMDBViewController.h
@@ -0,0 +1,13 @@
+//
+//  FMDBViewController.h
+//  Tarbar
+//
+//  Created by Qing Zhang on 5/3/16.
+//  Copyright © 2016 zhing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FMDBViewController : UIViewController
+
+@end
---
+++ Tarbar/Tarbar/FMDBViewController.m
@@ -0,0 +1,19 @@
+//
+//  FMDBViewController.m
+//  Tarbar
+//
+//  Created by Qing Zhang on 5/3/16.
+//  Copyright © 2016 zhing. All rights reserved.
+//
+
+#import "FMDBViewController.h"
+
+@interface FMDBViewController ()
+
+@property (nonatomic, strong) NSString *dbPath;
+
+@end
+
+@implementation FMDBViewController
+
+@end
