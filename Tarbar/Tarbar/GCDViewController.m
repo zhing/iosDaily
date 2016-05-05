a860a5dcd4129a8cc5fe4a2effce3959ead14670
zhing <zqwillseven@gmail.com>
2016-05-06 00:17:10 +0800

    deep in dispatch

--- Tarbar/Tarbar/GCDViewController.m
+++ Tarbar/Tarbar/GCDViewController.m
@@ -2,6 +2,8 @@
 //  GCDViewController.m
 //  Tarbar
 //
+//  https://github.com/nixzhu/dev-blog/blob/master/2014-04-19-grand-central-dispatch-in-depth-part-1.md
+//  https://github.com/nixzhu/dev-blog/blob/master/2014-05-14-grand-central-dispatch-in-depth-part-2.md
 //  Created by Qing Zhang on 5/5/16.
 //  Copyright © 2016 zhing. All rights reserved.
 //
@@ -29,7 +31,116 @@
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor whiteColor];
     
+    [self sync];
+    [self async];
     [self layoutUI];
+}
+
+- (void)dispatch{
+    /********************************
+    1. 用 dispatch_async 处理后台任务
+    *********************************/
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0), ^{
+        
+        /* 
+         do your background work
+         */
+        dispatch_async(dispatch_get_main_queue(), ^{
+            // fix your UI in mainThread
+        });
+    });
+    
+    /********************************
+    2.使用 dispatch_after 延后工作
+    *********************************/
+    dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.0f * NSEC_PER_SEC));
+    dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+        // do your delay job in mainThread
+    });
+    
+    /********************************
+    3.使用dispatch_once使单例更安全
+    *********************************/
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        // do your work which should be run just once
+    });
+    
+    /******************************
+    4.使用dispatch_group来同步线程
+    *******************************/
+//    __block NSError *error;
+//    dispatch_group_t downloadGroup = dispatch_group_create();
+//    
+//    for (NSInteger i = 0; i < 3; i++) {
+//        NSURL *url;
+//        switch (i) {
+//            case 0:
+//                url = [NSURL URLWithString:kOverlyAttachedGirlfriendURLString];
+//                break;
+//            case 1:
+//                url = [NSURL URLWithString:kSuccessKidURLString];
+//                break;
+//            case 2:
+//                url = [NSURL URLWithString:kLotsOfFacesURLString];
+//                break;
+//            default:
+//                break;
+//        }
+//        dispatch_group_enter(downloadGroup); // 2
+//        Photo *photo = [[Photo alloc] initwithURL:url
+//                              withCompletionBlock:^(UIImage *image, NSError *_error) {
+//                                  if (_error) {
+//                                      error = _error;
+//                                  }
+//                                  dispatch_group_leave(downloadGroup); // 3
+//                              }];
+//        
+//        [[PhotoManager sharedManager] addPhoto:photo];
+//    }
+//    
+//    dispatch_group_notify(downloadGroup, dispatch_get_main_queue(), ^{ // 4
+//        if (completionBlock) {
+//            completionBlock(error);
+//        }
+//    });
+    
+    /******************************
+     5.使用dispatch_semaphore来同步线程
+     *******************************/
+//    dispatch_semaphore_t semaphore = dispatch_semaphore_create(0);
+//    
+//    NSURL *url = [NSURL URLWithString:URLString];
+//    __unused Photo *photo = [[Photo alloc]
+//                             initwithURL:url
+//                             withCompletionBlock:^(UIImage *image, NSError *error) {
+//                                 if (error) {
+//                                     XCTFail(@"%@ failed. %@", URLString, error);
+//                                 }
+//                                 
+//                                 dispatch_semaphore_signal(semaphore);
+//                             }];
+//    
+//    dispatch_time_t timeoutTime = dispatch_time(DISPATCH_TIME_NOW, kDefaultTimeoutLengthInNanoSeconds);
+//    if (dispatch_semaphore_wait(semaphore, timeoutTime)) {
+//        XCTFail(@"%@ timed out", URLString);
+//    }
+}
+
+- (void)sync{
+    dispatch_sync(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0), ^{
+        NSLog(@"First sync Log");
+    });
+    
+    NSLog(@"Second sync Log");
+}
+
+-(void)async{
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0), ^{
+        NSLog(@"First async Log");
+    });
+    
+    NSLog(@"Second async Log");
 }
 
 - (void)layoutUI{
@@ -69,7 +180,7 @@
     [button3 setTitle:@"信号量" forState:UIControlStateNormal];
     [button3 setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
     [button3 addTarget:self action:@selector(loadImageWithMultiThreadConcurrent) forControlEvents:UIControlEventTouchUpInside];
-    [self.view addSubview:button2];
+    [self.view addSubview:button3];
     [button3 mas_makeConstraints:^(MASConstraintMaker *make) {
         make.bottom.equalTo(self.view).offset(-64-50);
         make.leading.equalTo(button1.mas_trailing).offset(15);
